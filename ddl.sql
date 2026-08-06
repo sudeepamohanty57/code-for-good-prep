c3b7f6f100bef68b0020ac0a1b6eeb651baaff45
sudeepamohanty57 <sudeepa.mohanty57@gmail.com>
2026-08-06 22:49:46 +0530

    Add DDL queries

--- ddl.sql
+++ ddl.sql
@@ -1,9 +1,24 @@
--- CREATE
-CREATE TABLE persons (
+-- =========================================
+-- DDL - Data Definition Language
+-- =========================================
+
+-- CREATE TABLE
+
+CREATE TABLE persons(
 	id INT NOT NULL,
-	person_name VARCHAR(50) NOT NULL,
+	first_name VARCHAR (50) NOT NULL,
 	birth_date DATE,
-	phone VARCHAR(15) NOT NULL,
-	CONSTRAINT pk_persons PRIMARY KEY (id)
+	phone VARCHAR (15),
+	CONSTRAINT pk_person PRIMARY KEY (id)
 )
+
+--ALTER TABLE
+
+ALTER TABLE persons
+ADD email VARCHAR(50) NOT NULL
+
+-- DROP
+-- Delete the table persons from the database
+
+DROP TABLE persons
 
