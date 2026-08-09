c75f7aa6bc82373f5740e1906ef33026e5a44649
sudeepamohanty57 <sudeepa.mohanty57@gmail.com>
2026-08-09 12:41:10 +0530

    Add dml queries

--- dml.sql
+++ dml.sql
@@ -22,3 +22,25 @@ SELECT
 	'Unknown'
 FROM customers
 
+
+--UPDATE table
+-- change the score of customer 6 to 0
+
+UPDATE customers
+SET score=0
+WHERE id=6
+
+-- change the score of customer with id 7 to 0 and update country to 'UK'
+
+UPDATE customers
+SET score=0,
+	country='UK'
+WHERE id=7
+
+--DELETE FROM table
+
+DELETE FROM customers
+where id>5
+
+--delete all the values from the table
+TRUNCATE TBALE persons
