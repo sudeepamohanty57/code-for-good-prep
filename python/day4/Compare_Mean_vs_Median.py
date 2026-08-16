99eaa95750779cc9d434711b38220078852eb649
sudeepamohanty57 <sudeepa.mohanty57@gmail.com>
2026-08-17 00:22:01 +0530

    Add Compare_Mean_vs_Median.py to python day4 practice

---
+++ python/day4/Compare_Mean_vs_Median.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+x = np.array([10, 11, 12, 13, 14])
+
+print("Mean:", np.mean(x))
+
+print("Median:", np.median(x))
+
+x = np.array([10, 11, 12, 13, 100])
+
+print("Mean:", np.mean(x))
+
+print("Median:", np.median(x))
