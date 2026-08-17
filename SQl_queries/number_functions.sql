76fe4dbfe468f971d732b164ac6af6e6db33e511
sudeepamohanty57 <sudeepa.mohanty57@gmail.com>
2026-08-17 13:02:20 +0530

    Add number_functions.sql to SQL_queries

---
+++ SQl_queries/number_functions.sql
@@ -0,0 +1,16 @@
+/* Number Functions */
+
+-- ROUND
+
+SELECT 
+3.516,
+ROUND(3.516, 2) AS round_2,
+ROUND(3.516, 1) As round_2,
+ROUND(3.516, 0) As round_0
+
+-- ABS
+
+SELECT
+-10,
+ABS(-10),
+ABS(10)
